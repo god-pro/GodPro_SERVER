a487b56a55f9e99cf441a5fa5e605480c539d13b
wooseob <dksdntjq3285@naver.com>
2024-10-05 17:36:48 +0900

    [Feat] ProjectController 생성

---
+++ src/main/java/org/godpro/godpro_server/domain/project/api/ProjectController.java
@@ -0,0 +1,10 @@
+package org.godpro.godpro_server.domain.project.api;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class ProjectController {
+
+}
