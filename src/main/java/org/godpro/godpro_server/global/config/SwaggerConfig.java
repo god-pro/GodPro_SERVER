9e5186b4037cd80c173becef0b1aa19e78dc89d6
MoonInbae <81071736+MoonInbae@users.noreply.github.com>
2024-10-05 11:58:44 +0900

    [Chore] Swagger Config 추가

---
+++ src/main/java/org/godpro/godpro_server/global/config/SwaggerConfig.java
@@ -0,0 +1,50 @@
+package org.godpro.godpro_server.global.config;
+
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+import lombok.RequiredArgsConstructor;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Configuration
+public class SwaggerConfig {
+
+    // application.yml에서 설정한 서버 URL을 읽어옴
+    @Value("${openapi.servers.url}")
+    private String serverUrl;
+
+    @Bean
+    public GroupedOpenApi chatOpenApi() {
+        String[] paths = {"/api/**"};
+
+        SecurityScheme cookieAuthScheme = new SecurityScheme()
+                .type(SecurityScheme.Type.APIKEY)
+                .in(SecurityScheme.In.COOKIE)
+                .name("session_id");
+
+        return GroupedOpenApi.builder()
+                .group("바당따라 Page API v1")
+                .pathsToMatch(paths)
+                .build();
+    }
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("바당따라")
+                        .description("바당따라 Api 명세서")
+                        .version("v1"))
+                .servers(List.of(new Server()
+                        .url(serverUrl)
+                        .description("Configured Server")));
+    }
+}
