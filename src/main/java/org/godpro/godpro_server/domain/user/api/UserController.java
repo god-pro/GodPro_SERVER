028743c272f6df1cc26a353402b8c42c75a67d68
MoonInbae <81071736+MoonInbae@users.noreply.github.com>
2024-10-05 21:47:57 +0900

    [Feat] User 조회 api 구현

--- src/main/java/org/godpro/godpro_server/domain/user/api/UserController.java
+++ src/main/java/org/godpro/godpro_server/domain/user/api/UserController.java
@@ -1,8 +1,12 @@
 package org.godpro.godpro_server.domain.user.api;
 
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.godpro.godpro_server.domain.user.application.UserService;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.godpro.godpro_server.domain.user.domain.User;
+import org.godpro.godpro_server.global.common.response.ApiResponse;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,5 +16,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
 
     private final UserService userService;
+
+    @Operation(summary = "카카오 아이디로 user정보 조회")
+    @GetMapping("/user")
+    public ApiResponse<User> retrieveUser(@RequestHeader("Authorization") String kakaoId) {
+        return userService.retrieveUser(kakaoId);
+    }
 
 }
