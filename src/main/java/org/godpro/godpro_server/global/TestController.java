00899ced32a8fe52ed9cbb850201dc555663973e
MoonInbae <81071736+MoonInbae@users.noreply.github.com>
2024-10-05 12:02:17 +0900

    [Refacfor] TestController 위치 변경

--- src/main/java/org/godpro/godpro_server/global/TestController.java
+++ src/main/java/org/godpro/godpro_server/global/TestController.java
@@ -1,4 +1,4 @@
-package org.godpro.godpro_server;
+package org.godpro.godpro_server.global;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
