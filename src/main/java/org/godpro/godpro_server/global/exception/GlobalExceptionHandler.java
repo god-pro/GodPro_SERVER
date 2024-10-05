7f20d084884f44b2929780ae6c49a198ca61df1c
MoonInbae <81071736+MoonInbae@users.noreply.github.com>
2024-10-05 12:20:59 +0900

    [Chore] GlobalExceptionHandler 추가

---
+++ src/main/java/org/godpro/godpro_server/global/exception/GlobalExceptionHandler.java
@@ -0,0 +1,35 @@
+package org.godpro.godpro_server.global.exception;
+
+import org.godpro.godpro_server.global.common.response.ApiResponse;
+import org.godpro.godpro_server.global.error.ErrorCode;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ApiResponse<String> handleBindException(MethodArgumentNotValidException exception) {
+		BindingResult bindingResult = exception.getBindingResult();
+		StringBuilder builder = new StringBuilder();
+
+		for (FieldError fieldError : bindingResult.getFieldErrors()) {
+			builder.append(" [");
+			builder.append(fieldError.getField());
+			builder.append("] -> ");
+			builder.append(fieldError.getDefaultMessage());
+			builder.append(" 입력된 값: [");
+			builder.append(fieldError.getRejectedValue());
+			builder.append("] ");
+		}
+
+		return ApiResponse.withError(ErrorCode.INVALID_INPUT_VALUE, builder.toString());
+	}
+
+}
