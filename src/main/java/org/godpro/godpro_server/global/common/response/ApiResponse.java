412772c2635abc7bc7a2066a368155a0dfca6e0a
MoonInbae <81071736+MoonInbae@users.noreply.github.com>
2024-10-05 12:00:59 +0900

    [Feat] ApiResponse 추가

---
+++ src/main/java/org/godpro/godpro_server/global/common/response/ApiResponse.java
@@ -0,0 +1,58 @@
+package org.godpro.godpro_server.global.common.response;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.alongtheblue.alongtheblue_server.global.error.ErrorCode;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class ApiResponse<T> {
+    // API 상태 코드
+    private int code;
+
+    // API 상태
+    private HttpStatus status;
+
+    // API 응답 메시지
+    private String message;
+
+    // API 응답 데이터
+    private T data;
+
+    private ApiResponse(HttpStatus status, String message, T data) {
+        this.code = status.value();
+        this.status = status;
+        this.message = message;
+        this.data = data;
+    }
+
+    public static <T> ApiResponse<T> of(HttpStatus status, String message, T data) {
+        return new ApiResponse<>(status, message, data);
+    }
+
+    public static <T> ApiResponse<T> of(HttpStatus status, T data) {
+        return of(status, status.name(), data);
+    }
+
+    public static <T> ApiResponse<T> ok(T data) {
+        return of(HttpStatus.OK, data);
+    }
+
+    public static <T> ApiResponse<T> ok(String message) {
+        return of(HttpStatus.OK, message, null);
+    }
+
+    public static <T> ApiResponse<T> ok(String message, T data) {
+        return of(HttpStatus.OK, message, data);
+    }
+
+    public static <T> ApiResponse<T> withError(ErrorCode errorCode, T data) {
+        return new ApiResponse<>(errorCode.getStatus(), errorCode.getMessage(), data);
+    }
+
+    public static <T> ApiResponse<T> withError(ErrorCode errorCode) {
+        return withError(errorCode, null);
+    }
+}
