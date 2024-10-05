790a71232558b1eddea6d98cfb0f67eef8fccaa6
BonSeung <bs.koo@sqisoft.com>
2024-10-06 04:16:36 +0900

    [Feat] 프로젝트 전체 조회 API 수정

--- src/main/java/org/godpro/godpro_server/domain/project/dao/ProjectRetrieve.java
+++ src/main/java/org/godpro/godpro_server/domain/project/dao/ProjectRetrieve.java
@@ -4,8 +4,10 @@ package org.godpro.godpro_server.domain.project.dao;
 public interface ProjectRetrieve {
   String getName();               // 프로젝트 이름
   String getShortDescription();   // 프로젝트 한 줄 소개
-  String getFront();                 // 프론트엔드 모집 인원
-  String getBack();                  // 백엔드 모집 인원
-  String getPm();                    // 기획자 모집 인원
-  String getDesign();                // 디자이너 모집 인원
+  int getFront();                 // 프론트엔드 모집 인원
+  int getBack();                  // 백엔드 모집 인원
+  int getPm();                    // 기획자 모집 인원
+  int getDesign();                // 디자이너 모집 인원
+  int getTotalRecruitment();
+  int getAcceptedApplicantCount();
 }
