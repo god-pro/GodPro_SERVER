40b5a09362dde5ce7b453543457628b8c326b3c6
MoonInbae <81071736+MoonInbae@users.noreply.github.com>
2024-10-05 17:10:23 +0900

    [Build] 빌드 설정

--- src/test/java/org/godpro/godpro_server/GodProServerApplicationTests.java
+++ src/test/java/org/godpro/godpro_server/GodProServerApplicationTests.java
@@ -3,7 +3,7 @@ package org.godpro.godpro_server;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class GodProServerApplicationTests {
 
     @Test
