2b350234b53b85380cac0676478fcd5bae2cce66
BonSeung <bs.koo@sqisoft.com>
2024-10-06 00:08:58 +0900

    [Feat] 프로젝트 지원자 합격 처리 API 추가

--- src/main/java/org/godpro/godpro_server/domain/application/application/ApplicationService.java
+++ src/main/java/org/godpro/godpro_server/domain/application/application/ApplicationService.java
@@ -7,7 +7,6 @@ import org.godpro.godpro_server.domain.application.dto.request.ApplyRequestDto;
 import org.godpro.godpro_server.domain.project.dao.ProjectRepository;
 import org.godpro.godpro_server.domain.project.domain.Project;
 import org.godpro.godpro_server.domain.user.application.UserService;
-import org.godpro.godpro_server.domain.user.dao.UserRepository;
 import org.godpro.godpro_server.domain.user.domain.User;
 import org.godpro.godpro_server.global.common.response.ApiResponse;
 import org.godpro.godpro_server.global.error.ErrorCode;
@@ -20,7 +19,6 @@ import java.util.Optional;
 @RequiredArgsConstructor
 public class ApplicationService {
 
-//    private final UserRepository userRepository;
     private final ProjectRepository projectRepository;
     private final ApplicationRepository applicationRepository;
     private final UserService userService;
@@ -38,8 +36,35 @@ public class ApplicationService {
         Project project = optionalProject.get();
 
         Application application = applyRequestDto.toEntity(project, user);
-        Application savedApplication = applicationRepository.save(application);
+        applicationRepository.save(application);
 
         return ApiResponse.ok("프로젝트에 성공적으로 지원하였습니다.");
+    }
+
+    public ApiResponse<String> acceptApplication(String userId, Long projectId, Long applicationId) {
+        ApiResponse<User> response = userService.retrieveUser(userId);
+        if(response.getStatus().equals(HttpStatus.NOT_FOUND)) return ApiResponse.withError(ErrorCode.INVALID_USER_ID);
+        User user = response.getData();
+
+        Optional<Project> optionalProject = projectRepository.findById(projectId);
+        if (optionalProject.isEmpty()) {
+            return ApiResponse.withError(ErrorCode.PROJECT_NOT_FOUND);
+        }
+        Project project = optionalProject.get();
+
+        Optional<Application> optionalApplication = applicationRepository.findById(applicationId);
+        if (optionalApplication.isEmpty()) {
+            return ApiResponse.withError(ErrorCode.APPLICATION_NOT_FOUND_ERROR);
+        }
+        Application application = optionalApplication.get();
+
+        if (project.getCreator().getId().equals(user.getId())) {
+            return ApiResponse.withError(ErrorCode.NOT_PROJECT_CREATOR_ERROR);
+        }
+
+        application.setAccepted(true);
+        applicationRepository.save(application);
+
+        return ApiResponse.ok("지원자가 성공적으로 합격 처리되었습니다.");
     }
 }
